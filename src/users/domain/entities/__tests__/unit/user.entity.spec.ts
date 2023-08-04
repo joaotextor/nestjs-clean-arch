9808986490901b4c8e6f2f831f5f4a2e80a9441f
João Textor <joaotextor@gmail.com>
2023-08-04 10:38:08 -0300

    test getters and create UserDataBuilder helper

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,16 +1,12 @@
-import { faker } from "@faker-js/faker";
 import { UserEntity, UserProps } from "../../user.entity";
+import { UserDataBuilder } from "@/users/domain/testing/helpers/user-data-builder";
 
 describe("UserEntity unit tests", () => {
   let props: UserProps;
   let sut: UserEntity;
 
   beforeEach(() => {
-    props = {
-      name: faker.person.fullName(),
-      email: faker.internet.email(),
-      password: faker.internet.password(),
-    };
+    props = UserDataBuilder({});
 
     sut = new UserEntity(props);
   });
@@ -19,5 +15,24 @@ describe("UserEntity unit tests", () => {
     expect(sut.props.email).toEqual(props.email);
     expect(sut.props.password).toEqual(props.password);
     expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+  it("Getter of name field", () => {
+    expect(sut.name).toBeDefined();
+    expect(sut.name).toEqual(props.name);
+    expect(typeof sut.name).toBe("string");
+  });
+  it("Getter of email field", () => {
+    expect(sut.email).toBeDefined();
+    expect(sut.email).toEqual(props.email);
+    expect(typeof sut.email).toBe("string");
+  });
+  it("Getter of password field", () => {
+    expect(sut.password).toBeDefined();
+    expect(sut.password).toEqual(props.password);
+    expect(typeof sut.password).toBe("string");
+  });
+  it("Getter of createdAt field", () => {
+    expect(sut.createdAt).toBeDefined();
+    expect(sut.createdAt).toBeInstanceOf(Date);
   });
 });
