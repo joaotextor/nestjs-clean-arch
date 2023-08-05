76884e2a842bea3ce0be13fad4920b0faf7df166
João Textor <joaotextor@gmail.com>
2023-08-05 10:27:48 -0300

    unit tests for setters and update methods

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -21,6 +21,11 @@ describe("UserEntity unit tests", () => {
     expect(sut.name).toEqual(props.name);
     expect(typeof sut.name).toBe("string");
   });
+  it("Setter of name field", () => {
+    sut["name"] = "new name";
+    expect(sut.name).toEqual("new name");
+    expect(typeof sut.name).toBe("string");
+  });
   it("Getter of email field", () => {
     expect(sut.email).toBeDefined();
     expect(sut.email).toEqual(props.email);
@@ -31,8 +36,21 @@ describe("UserEntity unit tests", () => {
     expect(sut.password).toEqual(props.password);
     expect(typeof sut.password).toBe("string");
   });
+  it("Setter of password field", () => {
+    sut["password"] = "new password";
+    expect(sut.password).toEqual("new password");
+    expect(typeof sut.password).toBe("string");
+  });
   it("Getter of createdAt field", () => {
     expect(sut.createdAt).toBeDefined();
     expect(sut.createdAt).toBeInstanceOf(Date);
+  });
+  it("Should update a user name", () => {
+    sut.update("new name");
+    expect(sut.name).toEqual("new name");
+  });
+  it("Should update password", () => {
+    sut.update("new password");
+    expect(sut.name).toEqual("new password");
   });
 });
