0433208a292ccc93f7a0280df27ccceaa946cd6f
João Textor <joaotextor@gmail.com>
2023-08-07 10:40:08 -0300

    Project documentation - in progress

--- src/shared/infrastructure/env-config/env-config.interface.ts
+++ src/shared/infrastructure/env-config/env-config.interface.ts
@@ -1,3 +1,8 @@
+/*
+ * This interface is used to get the App Port and Environment variables
+ * @public
+ */
+
 export interface EnvConfig {
   getAppPort(): number;
   getNodeEnv(): string;
--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -2,13 +2,33 @@ import { Injectable } from "@nestjs/common";
 import { EnvConfig } from "./env-config.interface";
 import { ConfigService } from "@nestjs/config";
 
+/*
+ * Configure the service to configure environment variables
+ * @public
+ 
+ */
+
 @Injectable()
 export class EnvConfigService implements EnvConfig {
+  /*
+   * @remarks Inject the ConfigService class
+   * @param configService
+   */
+
   constructor(private configService: ConfigService) {}
 
+  /*
+   * Get the port number
+   * @returns PORT - App port number
+   */
   getAppPort(): number {
     return Number(this.configService.get<number>("PORT"));
   }
+
+  /*
+   * Get the node environment
+   * @returns NODE_ENV - Node environment
+   */
   getNodeEnv(): string {
     return this.configService.get<string>("NODE_ENV");
   }
