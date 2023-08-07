9c0f22e467d547f7b71aa37be5a9a7b3853412bd
João Textor <joaotextor@gmail.com>
2023-08-07 18:54:49 -0300

    unit tests for UserValidator

--- src/shared/infrastructure/env-config/env-config.module.ts
+++ src/shared/infrastructure/env-config/env-config.module.ts
@@ -6,8 +6,21 @@ import { join } from "node:path";
 @Module({
   providers: [EnvConfigService],
 })
+/*
+ * Create a class based on the ConfigModule class from nest.js
+ */
 export class EnvConfigModule extends ConfigModule {
+  /*
+   * replicate the forRoot method from ConfigModule
+   * @param options - ConfigModuleOptions
+   * @returns a DynamicModule - global accessable module
+   */
   static forRoot(options: ConfigModuleOptions = {}): DynamicModule {
+    /*
+     * @returns forRoot method from parent class
+     * @param options
+     * @param envFilePath -
+     */
     return super.forRoot({
       ...options,
       envFilePath: [
