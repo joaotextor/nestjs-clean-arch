ae83544f15fcc86c3549a1bd486bbeec37b6a4f9
João Textor <joaotextor@gmail.com>
2023-08-26 09:53:37 -0300

    Adding SearchParams class

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -1,10 +1,68 @@
 import { Entity } from "../entities/entity";
 import { RepositoryInterface } from "./repository-contracts";
 
+export type SortDirection = "asc" | "desc";
+
+export type SearchProps<Filter = string> = {
+  page?: number;
+  perPage?: number;
+  sort?: string | null;
+  sortDir?: SortDirection | null;
+  filter?: Filter | null;
+};
+
+export class SearchParams {
+  protected _page: number;
+  protected _perPage = 15;
+  protected _sort: string | null;
+  protected _sortDir: SortDirection | null;
+  protected _filter: string | null;
+
+  constructor(props: SearchProps) {
+    this._page = props.page;
+    this._perPage = props.perPage;
+    this._sort = props.sort;
+    this._sortDir = props.sortDir;
+    this._filter = props.filter;
+  }
+
+  get page() {
+    return this._page;
+  }
+
+  private set page(value: number) {
+    this._page = value;
+  }
+
+  get perPage() {
+    return this._perPage;
+  }
+
+  private set perPage(value: number) {}
+
+  get sort() {
+    return this._sort;
+  }
+
+  private set sort(value: string | null) {}
+
+  get sortDir() {
+    return this._sortDir;
+  }
+
+  private set sortDir(value: SortDirection | null) {}
+
+  get filter() {
+    return this._filter;
+  }
+
+  private set filter(value: string | null) {}
+}
+
 export interface SearchableRepositoryInterface<
   E extends Entity,
-  SearchInput,
+  SearchParams,
   SearchOutput,
 > extends RepositoryInterface<E> {
-  search(search: SearchInput): Promise<SearchOutput>;
+  search(search: SearchParams): Promise<SearchOutput>;
 }
