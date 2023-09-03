36abffb54a7d179637774597a2ddcf59ff153a60
João Textor <joaotextor@gmail.com>
2023-09-03 16:32:15 -0300

    refactor user repository

--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -1,13 +1,16 @@
 import { ConflictError } from "@/shared/domain/errors/conflict-error";
 import { NotFoundError } from "@/shared/domain/errors/not-found-error";
 import { SearchableInMemoryRepository } from "@/shared/domain/repositories/searchable-in-memory.repository";
+import { SortDirection } from "@/shared/domain/repositories/searchable-repository-contracts";
 import { UserEntity } from "@/users/domain/entities/user.entity";
 import { UserRepository } from "@/users/domain/repositories/user.repository";
 
 export class UserInMemoryRepository
   extends SearchableInMemoryRepository<UserEntity>
-  implements UserRepository
+  implements UserRepository.Repository
 {
+  sortableFields: string[] = ["name", "createdAt"];
+
   async findByEmail(email: string): Promise<UserEntity> {
     const entity = this.items.find((item) => item.email === email);
     if (!entity) {
@@ -20,5 +23,28 @@ export class UserInMemoryRepository
     if (entity) {
       throw new ConflictError("Email address already used");
     }
+  }
+
+  protected async applyFilter(
+    items: UserEntity[],
+    filter: UserRepository.Filter,
+  ): Promise<UserEntity[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return items.filter((item) => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase());
+    });
+  }
+
+  protected async applySort(
+    items: UserEntity[],
+    sort: string | null,
+    sortDir: SortDirection | null,
+  ): Promise<UserEntity[]> {
+    return !sort
+      ? super.applySort(items, "createdAt", "desc")
+      : super.applySort(items, sort, sortDir);
   }
 }
