4d5e4cba82e9da9b0d7efde1256a6a2b34dd193b
João Textor <joaotextor@gmail.com>
2023-10-24 09:23:10 -0300

    test: `UserPresenterCollection` unit tests

--- src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
+++ src/users/infrastructure/presenters/__tests__/unit/user.presenter.spec.ts
@@ -1,5 +1,6 @@
 import { instanceToPlain } from "class-transformer";
-import { UserPresenter } from "../../user.presenter";
+import { UserCollectionPresenter, UserPresenter } from "../../user.presenter";
+import { PaginationPresenter } from "@/shared/infrastructure/presenters/pagination.presenter";
 
 describe("UserPresenter unit tests", () => {
   const createdAt = new Date();
@@ -32,6 +33,95 @@ describe("UserPresenter unit tests", () => {
       name: "Test Name",
       email: "a@a.com",
       createdAt: createdAt.toISOString(),
+    });
+  });
+});
+
+describe("UserCollectionPresenter unit tests", () => {
+  describe("constructor", () => {
+    const createdAt = new Date();
+    const props = {
+      id: "c4583bac-a75f-431d-9ba3-adf91ed686dc",
+      name: "Test Name",
+      email: "a@a.com",
+      password: "fake-password",
+      createdAt,
+    };
+
+    it("should set values", () => {
+      const sut = new UserCollectionPresenter({
+        items: [props],
+        currentPage: 1,
+        perPage: 2,
+        lastPage: 1,
+        total: 1,
+      });
+
+      expect(sut.meta).toBeInstanceOf(PaginationPresenter);
+      expect(sut.meta).toStrictEqual(
+        new PaginationPresenter({
+          currentPage: 1,
+          perPage: 2,
+          lastPage: 1,
+          total: 1,
+        }),
+      );
+      expect(sut.data).toStrictEqual([new UserPresenter(props)]);
+    });
+    it("should set values", () => {
+      let sut = new UserCollectionPresenter({
+        items: [props],
+        currentPage: 1,
+        perPage: 2,
+        lastPage: 1,
+        total: 1,
+      });
+
+      let output = instanceToPlain(sut);
+
+      expect(output).toMatchObject({
+        data: [
+          {
+            id: "c4583bac-a75f-431d-9ba3-adf91ed686dc",
+            name: "Test Name",
+            email: "a@a.com",
+            createdAt: createdAt.toISOString(),
+          },
+        ],
+        meta: {
+          currentPage: 1,
+          perPage: 2,
+          lastPage: 1,
+          total: 1,
+        },
+      });
+
+      sut = new UserCollectionPresenter({
+        items: [props],
+        currentPage: "1" as any,
+        perPage: "2" as any,
+        lastPage: "1" as any,
+        total: "1" as any,
+      });
+
+      output = instanceToPlain(sut);
+
+      expect(output).toMatchObject({
+        data: [
+          {
+            id: "c4583bac-a75f-431d-9ba3-adf91ed686dc",
+            name: "Test Name",
+            email: "a@a.com",
+            createdAt: createdAt.toISOString(),
+          },
+        ],
+        meta: {
+          currentPage: 1,
+          perPage: 2,
+          lastPage: 1,
+          total: 1,
+        },
+      });
     });
   });
 });
--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -24,7 +24,10 @@ import { SigninDto } from "./dtos/signin.dto";
 import { ListUsersDto } from "./dtos/list-users.dto";
 import { UpdatePasswordDto } from "./dtos/update-password.dto";
 import { UserOutput } from "../application/dto/user-output";
-import { UserPresenter } from "./presenters/user.presenter";
+import {
+  UserCollectionPresenter,
+  UserPresenter,
+} from "./presenters/user.presenter";
 
 @Controller("users")
 export class UsersController {
@@ -53,6 +56,10 @@ export class UsersController {
     return new UserPresenter(output);
   }
 
+  static listUsersToResponse(output: ListUsersUseCase.Output) {
+    return new UserCollectionPresenter(output);
+  }
+
   @Post()
   async create(@Body() signupDto: SignupDto) {
     const output = await this.signupUseCase.execute(signupDto);
@@ -68,7 +75,8 @@ export class UsersController {
 
   @Get()
   async search(@Query() SearchParams: ListUsersDto) {
-    return this.listUsersUseCase.execute(SearchParams);
+    const output = await this.listUsersUseCase.execute(SearchParams);
+    return UsersController.listUsersToResponse(output);
   }
 
   @Get(":id")
